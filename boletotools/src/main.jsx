9a14a86cca2774f29c099e85c9aaf0604646c03a
matheusedivado <ms1501085@gmail.com>
2024-11-03 19:51:44 -0300

    Fontes e Paleta de Cores

--- boletotools/src/main.jsx
+++ boletotools/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//Fontes
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/700.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
